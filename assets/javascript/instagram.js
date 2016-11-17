f7c7247b796127c622bce55d608dad1a081c863e
jesseharold <jesseharold@gmail.com>
2016-11-16 23:46:16 -0800

    add cookie storage of instagram auth token

--- assets/javascript/instagram.js
+++ assets/javascript/instagram.js
@@ -19,7 +19,20 @@ if (location.href.indexOf("#") > 0){
     if (authToken[0] === "access_token"){
         $("#login").hide();
         token = authToken[1];
+        document.cookie = "authToken=" + token;
         initializeApp();
+    }
+} else {
+    //look for token value in cookies
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++){
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf("authToken=") === 0) {
+            token = cookie.substring(10, cookie.length);
+        }
     }
 }
 
