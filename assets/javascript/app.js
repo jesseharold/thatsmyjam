e4883dfa90e36876877cf8fe0d1cafd15da52afe
Eric Hirshfield <erichirshfield@Erics-MacBook-Air-2.local>
2016-11-17 00:17:48 -0800

    thumbs up toggle works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,6 +22,31 @@ $(document).on("click","#submit-restaurant", function(){
     window.location = "index.html";
 });
 
+//Color the thumbs up or down
+$(document).on("click","#thumbup", function(){
+    
+    
+    if ($(this).hasClass('mdl-button mdl-js-button mdl-button--icon')){
+    $(this).toggleClass('mdl-button--colored');
+    componentHandler.upgradeDom();
+    
+    }
+
+    
+});
+
+$(document).on("click","#thumbdown", function(){
+    
+    
+    if ($(this).hasClass('mdl-button mdl-js-button mdl-button--icon')){
+    $(this).toggleClass('mdl-button--colored');
+    componentHandler.upgradeDom();
+    
+    }
+
+    
+});
+
 
 
 function populateFriendsList() {
