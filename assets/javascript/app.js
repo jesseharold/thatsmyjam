eaf76ff84c012f05763c6eb9ed27c24e9e3b6440
jesseharold <jesseharold@gmail.com>
2016-11-19 01:28:14 -0800

    adding expected data structure to new review

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,6 +45,9 @@ function addReviewModal(){
             images: {
                 thumbnail: {},
                 standard_resolution: {}
+            },
+            caption: {
+                from: {}
             }
         };
         reviewData.text = $(".modalContainer #reviewText").val();
