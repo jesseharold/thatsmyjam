8f03ace80bf0525ff02521e5ab6f507360f36c2f
jesseharold <jesseharold@gmail.com>
2016-11-19 00:34:11 -0800

    moved modals to app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,28 @@ $('.mdl-layout__obfuscator-right').click(function(){
 $(document).on("click",".add-review-link", function(){
     addReviewModal();
 });
+function addReviewModal(){
+    var formContainer = $("<div>");
+    formContainer.append("<h3>Add a Review</h3>");
+    formContainer.append("Restaurant Name: <input type='text' id='restaurantName'>");
+    formContainer.append("<br>Thumbs: Up <input type='radio' name='thumb' value='up' id='thumbsUp'>");
+    formContainer.append("Down <input type='radio' name='thumb' value='down' id='thumbsDown'>");
+    formContainer.append("<br>Review: <input type='text' id='reviewText'>");
+    formContainer.append("<br>Location: <button id='useLocation'>Use Current Location</button><button id='enterAddress'>Enter Address</button>");
+    openModal(formContainer);
+    $("body").on("click", ".modalBG", function(){
+        $("div.modalContainer").remove();
+    });
+}
+function openModal(content){
+    var modalContainer = $("<div>").addClass("modalContainer");
+    var modalBG = $("<div>").addClass("modalBG");
+    var modalContent = $("<div>").addClass("modalContent");
+    modalContainer.append(modalBG);
+    modalContainer.append(modalContent);
+    modalContent.append(content);
+    $("body").append(modalContainer);
+}
 
 //Color the thumbs up or down
 $(document).on("click","#thumbup", function(){
--- assets/javascript/instagram.js
+++ assets/javascript/instagram.js
@@ -326,32 +326,6 @@ function addReviewAndNewRestaurant(imageData){
     database.ref("restaurants").push(thisRestaurant);
 }//function addReviewAndNewRestaurant
 
-
-function addReviewModal(){
-    var formContainer = $("<div>");
-    formContainer.append("<h3>Add a Review</h3>");
-    formContainer.append("Restaurant Name: <input type='text' id='restaurantName'>");
-    formContainer.append("<br>Thumbs: Up <input type='radio' name='thumb' value='up' id='thumbsUp'>");
-    formContainer.append("Down <input type='radio' name='thumb' value='down' id='thumbsDown'>");
-    formContainer.append("<br>Review: <input type='text' id='reviewText'>");
-    formContainer.append("<br>Location: <button id='useLocation'>Use Current Location</button><button id='enterAddress'>Enter Address</button>");
-    openModal(formContainer);
-    $("body").on("click", ".modalBG", function(){
-        $("div.modalContainer").remove();
-    });
-
-}
-
-function openModal(content){
-    var modalContainer = $("<div>").addClass("modalContainer");
-    var modalBG = $("<div>").addClass("modalBG");
-    var modalContent = $("<div>").addClass("modalContent");
-    modalContainer.append(modalBG);
-    modalContainer.append(modalContent);
-    modalContent.append(content);
-    $("body").append(modalContainer);
-}
-
 $("document").ready(function(){
 checkForAuthToken();
 });//document ready
