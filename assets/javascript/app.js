42c1022b7c6eceaf3ed911b96b1b56b38db1a815
jesseharold <jesseharold@gmail.com>
2016-11-19 00:32:04 -0800

    adding click event to open review modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,32 +17,23 @@ $('.mdl-layout__obfuscator-right').click(function(){
 });
 
 
-$(document).on("click","#submit-restaurant", function(){
-    window.location = "index.html";
+$(document).on("click",".add-review-link", function(){
+    addReviewModal();
 });
 
 //Color the thumbs up or down
 $(document).on("click","#thumbup", function(){
-    
-    
     if ($(this).hasClass('mdl-button mdl-js-button mdl-button--icon')){
     $(this).toggleClass('mdl-button--colored');
     componentHandler.upgradeDom();
-    
     }
-
-    
 });
 
 $(document).on("click","#thumbdown", function(){
-    
-    
     if ($(this).hasClass('mdl-button mdl-js-button mdl-button--icon')){
     $(this).toggleClass('mdl-button--colored');
     componentHandler.upgradeDom();
-    
     }
-    
 });
 
 
